80f944a1ae135709198bc0b4c848c4f26f382558
Junbum Lee <beomi@snu.ac.kr>
2021-06-14 03:29:34 +0000

    Add kowiki train sh

--- examples/pytorch/language-modeling/exbert_pretrain_kowiki.sh
+++ examples/pytorch/language-modeling/exbert_pretrain_kowiki.sh
@@ -5,11 +5,11 @@ python run_mlm.py \
 --seed 42 \
 --model_type exbert \
 --tokenizer_name exbert \
---train_file paws_corpus.txt \
---num_train_epochs 10 \
+--train_file kowikitext.txt \
+--num_train_epochs 2 \
 --per_device_train_batch_size $BS \
 --per_device_eval_batch_size $BS \
 --do_train \
---output_dir ./exbert-mlm \
+--output_dir ./exbert-kowiki \
 --logging_first_step \
 --max_seq_length 300 \
